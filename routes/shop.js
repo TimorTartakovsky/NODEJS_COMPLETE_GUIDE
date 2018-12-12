89f5e44942bdcc51901c6458055ab64151517ee5
Timor <timor.tartakovsky@controlup.com>
2018-12-12 12:29:34 +0200

    another commit

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,18 @@
-const express =  require('express');
-const router  = express.Router();
-const path  = require('path');
-const rootDir = require('../utils/path');
+const express = require('express');
+const adminData = require('./admin');
 
-router.get('/', (req, res) => {
-	res.sendFile(`${path.join(rootDir, 'views', 'shop.html')}`);
-	// res.send("<h1>Hello this is my main page</h1>");
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
